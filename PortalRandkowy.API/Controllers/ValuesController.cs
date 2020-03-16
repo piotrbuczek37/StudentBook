1666a9f7ae7a36ec3651c2c5066560a8980f8312
Piotr Buczek <piotr.buczek37@gmail.com>
2020-03-16 19:21:59 +0200

    Stworzenie metod do operacji na danych

--- PortalRandkowy.API/Controllers/ValuesController.cs
+++ PortalRandkowy.API/Controllers/ValuesController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using PortalRandkowy.API.Data;
+using PortalRandkowy.API.Models;
 
 namespace PortalRandkowy.API.Controllers
 {
@@ -10,9 +12,56 @@ namespace PortalRandkowy.API.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
+        private readonly DataContext _context;
+        public ValuesController(DataContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get(){
-            return new string[] {"value1","value2"};
+        public IActionResult GetValues()
+        {
+            var values = _context.Values.ToList();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetValue(int id)
+        {
+            var value = _context.Values.FirstOrDefault(x => x.Id == id);
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public IActionResult AddValue([FromBody] Value value)
+        {
+            _context.Values.Add(value);
+            _context.SaveChanges();
+            return Ok(value);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult EditValue(int id, [FromBody] Value value)
+        {
+            var data = _context.Values.Find(id);
+            data.Name = value.Name;
+            _context.Values.Update(data);
+            _context.SaveChanges();
+            return Ok(data);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteValue(int id)
+        {
+            var data = _context.Values.Find(id);
+
+            if(data == null)
+            return NoContent();
+            else{
+            _context.Values.Remove(data);
+            _context.SaveChanges();
+            return Ok(data);
+            }
         }
     }
 }
