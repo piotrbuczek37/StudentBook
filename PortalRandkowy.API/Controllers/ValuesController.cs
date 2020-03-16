99c09249717c3b1f84967744d6435f3dec3acca7
Piotr Buczek <piotr.buczek37@gmail.com>
2020-03-16 14:52:08 +0200

    Utworzenie modelu Value

---
+++ PortalRandkowy.API/Controllers/ValuesController.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PortalRandkowy.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get(){
+            return new string[] {"value1","value2"};
+        }
+    }
+}
--- PortalRandkowy.API/Startup.cs
+++ PortalRandkowy.API/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using PortalRandkowy.API.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace PortalRandkowy.API
 {
@@ -25,6 +27,7 @@ namespace PortalRandkowy.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DataContext>(x => x.UseSqlite("Connectionstring"));
             services.AddControllers();
         }
 
