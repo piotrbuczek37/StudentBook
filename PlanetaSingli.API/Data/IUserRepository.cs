866f106fcc6d733605344fac7828d13de7448ca4
Piotr Buczek <piotr.buczek37@gmail.com>
2020-04-05 17:01:07 +0500

    Dodano interfejsy do repozytoriów

---
+++ PlanetaSingli.API/Data/IUserRepository.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using PlanetaSingli.API.Models;
+
+namespace PlanetaSingli.API.Data
+{
+    public interface IUserRepository : IGenericRepository
+    {
+         Task<IEnumerable<User>> GetUsers();
+         Task<User> GetUser(int id);
+    }
+}
