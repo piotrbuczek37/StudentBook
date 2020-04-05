15d41cf351e574f979090f7f5bbd9688f9d89890
Piotr Buczek <piotr.buczek37@gmail.com>
2020-04-05 18:06:43 +0500

    Dodano Dtos

---
+++ PlanetaSingli.API/Dtos/UserForDetailsDto.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using PlanetaSingli.API.Models;
+
+namespace PlanetaSingli.API.Dtos
+{
+    public class UserForDetailsDto
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+
+        //Informacje o użytkowniku
+        public string Gender { get; set; }
+        public int Age { get; set; }
+        public string ZodiacSign { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+
+        //Dodatkowe informacje
+        public string Growth { get; set; }
+        public string EyeColor { get; set; }
+        public string HairColor { get; set; }
+        public string MartialStatus { get; set; }
+        public string Education { get; set; }
+        public string Profession { get; set; }
+        public string Children { get; set; }
+        public string Languages { get; set; }
+
+        //Zakładka O mnie
+        public string Motto { get; set; }
+        public string Description { get; set; }
+        public string Personality { get; set; }
+        public string LookingFor { get; set; }
+
+        //Pasje, zainteresowania
+        public string Interests { get; set; }
+        public string FreeTime { get; set; }
+        public string Sport { get; set; }
+        public string Movies { get; set; }
+        public string Music { get; set; }
+
+        //Preferencje użytkownika
+        public string ILike { get; set; }
+        public string IdoNotLike { get; set; }
+        public string MakesMeLaugh { get; set; }
+        public string ItFeelsBestIn { get; set; }
+        public string FriendsWouldDescribeMe { get; set; }
+
+        //Zdjęcia
+        public ICollection<Photo> Photos { get; set; }
+        public string PhotoUrl {get; set;}
+    }
+}
