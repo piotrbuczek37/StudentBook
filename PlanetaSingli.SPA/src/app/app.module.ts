84682be0a54a5e6ecf0b05772b5cfe17995fc423
Piotr Buczek <piotr.buczek37@gmail.com>
2020-04-06 13:43:38 +0500

    Dodano serwis do users

--- PlanetaSingli.SPA/src/app/app.module.ts
+++ PlanetaSingli.SPA/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { AuthService } from './_services/auth.service';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { AlertifyService } from './_services/alertify.service';
+import { UserService } from './_services/user.service';
 
 @NgModule({
    declarations: [
@@ -25,7 +26,9 @@ import { AlertifyService } from './_services/alertify.service';
       FormsModule
    ],
    providers: [
-      AuthService, AlertifyService
+      AuthService, 
+      AlertifyService,
+      UserService
    ],
    bootstrap: [
       AppComponent
