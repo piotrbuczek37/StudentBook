66b2200bcce71a0d4440ce72972dd7b401ba43bf
Piotr Buczek <piotr.buczek37@gmail.com>
2020-04-19 13:40:45 +0200

    Poprawiono paginację w API

--- PlanetaSingli.API/Data/IUserRepository.cs
+++ PlanetaSingli.API/Data/IUserRepository.cs
@@ -1,12 +1,13 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using PlanetaSingli.API.Helpers;
 using PlanetaSingli.API.Models;
 
 namespace PlanetaSingli.API.Data
 {
     public interface IUserRepository : IGenericRepository
     {
-         Task<IEnumerable<User>> GetUsers();
+         Task<PagedList<User>> GetUsers(UserParams userParams);
          Task<User> GetUser(int id);
          Task<Photo> GetPhoto(int id);
          Task<Photo> GetMainPhotoForUser(int userId);
--- PlanetaSingli.API/Data/UserRepository.cs
+++ PlanetaSingli.API/Data/UserRepository.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using PlanetaSingli.API.Helpers;
 using PlanetaSingli.API.Models;
 
 namespace PlanetaSingli.API.Data
@@ -32,10 +33,10 @@ namespace PlanetaSingli.API.Data
             return user;
         }
 
-        public async Task<IEnumerable<User>> GetUsers()
+        public async Task<PagedList<User>> GetUsers(UserParams userParams)
         {
-            var users = await _context.Users.Include(p => p.Photos).ToListAsync();
-            return users;
+            var users = _context.Users.Include(p => p.Photos);
+            return await PagedList<User>.CreateListAsync(users, userParams.PageNumber, userParams.PageSize);
         }
     }
 }
