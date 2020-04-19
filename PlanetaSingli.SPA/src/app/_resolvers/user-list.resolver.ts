a0ecf3c2c3c4ddbd56a42677fc8d4e74a329197e
Piotr Buczek <piotr.buczek37@gmail.com>
2020-04-19 13:52:22 +0200

    Dodano paginacje do SPA

--- PlanetaSingli.SPA/src/app/_resolvers/user-list.resolver.ts
+++ PlanetaSingli.SPA/src/app/_resolvers/user-list.resolver.ts
@@ -9,10 +9,13 @@ import { catchError } from 'rxjs/operators';
 @Injectable()
 export class UserListResolver implements Resolve<User[]> {
     
+    pageNumber = 1;
+    pageSize = 42;
+
     constructor(private userService: UserService, private router: Router, private alertify: AlertifyService) {}
 
     resolve(route: ActivatedRouteSnapshot): Observable<User[]>{
-        return this.userService.getUsers().pipe(
+        return this.userService.getUsers(this.pageNumber, this.pageSize).pipe(
             catchError(error => {
                 this.alertify.error(error);
                 this.router.navigate(['']);
