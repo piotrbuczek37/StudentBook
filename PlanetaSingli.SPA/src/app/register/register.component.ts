789dc1d60add90902949ec7fab4a8c79b9b09232
Piotr Buczek <piotr.buczek37@gmail.com>
2020-04-19 09:58:21 +0200

    Poprawiono rejestrację w SPA

--- PlanetaSingli.SPA/src/app/register/register.component.ts
+++ PlanetaSingli.SPA/src/app/register/register.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { User } from '../_models/user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -11,10 +13,10 @@ import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
 export class RegisterComponent implements OnInit {
 
   @Output() cancelRegister = new EventEmitter();
-  model: any = {};
+  user: User;
   registerForm: FormGroup;
 
-  constructor(private authService: AuthService, private alertify: AlertifyService, private fb: FormBuilder) { }
+  constructor(private authService: AuthService, private alertify: AlertifyService, private fb: FormBuilder, private router: Router) { }
 
   ngOnInit() {
     this.createRegisterForm();
@@ -38,12 +40,18 @@ export class RegisterComponent implements OnInit {
   }
 
   register(){
-    // this.authService.register(this.model).subscribe( () => {
-    //   this.alertify.success('Zarejestrowano');
-    // }, error => {
-    //   this.alertify.error(error);
-    // });
-    console.log(this.registerForm.value);
+    if(this.registerForm.valid){
+      this.user = Object.assign({}, this.registerForm.value);
+      this.authService.register(this.user).subscribe( () => {
+      this.alertify.success('Zarejestrowano');
+    }, error => {
+      this.alertify.error(error);
+      }, () => {
+        this.authService.login(this.user).subscribe( () => {
+          this.router.navigate(['/uzytkownicy']);
+        });
+      });
+    }
   }
 
   cancel(){
